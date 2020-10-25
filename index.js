6eaf44fea0b6bd71557821e2a5e24ce50634f1a7
Brian Kubes <briankubes@gmail.com>
2020-10-25 12:43:37 -0700

    get request done

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 require('dotenv').config()
 const express = require('express')
-
+const cors = require('cors')
+const uuid = require('uuid')
 const server = express()
 
 server.use(express.json())
+server.use(cors())
+
+//Here is some seed data
+
+let dogs = [ 
+    {
+        id: uuid.v4(),
+        breed: 'German Shepard',
+        imageUrl: 'https://images.unsplash.com/photo-1521229424090-c4e9ff91d66b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2549&q=80'
+    }
+]
+
+// Get all dogs
+
+server.get('/dogs', (req,res)=> {
+    try{
+        res.status(200).json(dogs)
+    }
+    catch {
+        res.status(500).json({ errorMessage: "Sorry cannot get dogs" })
+    }
+})
+
 
 server.use('/', (req, res) => {
     res.status(200).json({message: 'Hello Lambda Students'})
